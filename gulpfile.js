77b7b7a6082ff987dcb3d9bc3db054c14cb41564
nobe4 <haffreinguev@gmail.com>
2015-12-14 13:27:29 +0100

    Add gulp-plumber to test suite

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jasmine = require('gulp-jasmine');
+var plumber = require('gulp-plumber');
 
 gulp.task('jshint', function(){
-	 gulp.src(['src/*.js', 'test/*.js'])
+	gulp.src(['src/*.js', 'test/*.js'])
+	.pipe(plumber())
 	.pipe(jshint())
 	.pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('test', function(){
-	 gulp.src('test/palindrone.js')
+	gulp.src('test/palindrone.js')
+	.pipe(plumber())
 	.pipe(jasmine());
 });
 
--- test/palindrone.js
+++ test/palindrone.js
@@ -1,10 +1,14 @@
 var palindrone = require('../src/palindrone.js');
-describe("Palindrone test suite", function() {
-	it('should detect palindromic sequences', function(){
-		expect(palindrone.fn.isSequencePalindromic('a'))
-		.toBe(true);
+describe('Palindrone test suite', function() {
 
-		expect(palindrone.fn.isSequencePalindromic('aba'))
+	palindrone.searchedSequence= 'sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop';
+
+	it('should detect palindromic sequences', function(){
+		// qrrq
+		expect(palindrone.fn.isSequencePalindromic(1,4))
 		.toBe(true);
+		// sqrrq
+		expect(palindrone.fn.isSequencePalindromic(0,4))
+		.toBe(false);
 	});
 });
