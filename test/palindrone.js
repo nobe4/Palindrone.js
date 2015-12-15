3072ba66482449d8bc8ad49d6992d31135d8c17d
nobe4 <haffreinguev@gmail.com>
2015-12-15 21:26:09 +0100

    Add new tests

--- test/palindrone.js
+++ test/palindrone.js
@@ -1,14 +1,39 @@
 var palindrone = require('../src/palindrone.js');
 describe('Palindrone test suite', function() {
 	it('should get a list of palindromic sequences', function(){
+		palindrone.fn.init('');
+		palindrone.fn.findPalindromicSequences();
+		expect(palindrone.foundPalindromicSequences.length)
+		.toBe(0);
+
+		palindrone.fn.init('no palindrome');
+		palindrone.fn.findPalindromicSequences();
+		expect(palindrone.foundPalindromicSequences.length)
+		.toBe(0);
+
+		palindrone.fn.init('only 1 palindrome');
+		palindrone.fn.findPalindromicSequences();
+		expect(palindrone.foundPalindromicSequences.length)
+		.toBe(1);
+
+		palindrone.fn.init('this string has 2 palindromes in it');
+		palindrone.fn.findPalindromicSequences();
+		expect(palindrone.foundPalindromicSequences.length)
+		.toBe(2);
+
 		palindrone.fn.init('this string has 3 palindromes in it, kayak');
 		palindrone.fn.findPalindromicSequences();
 		expect(palindrone.foundPalindromicSequences.length)
 		.toBe(3);
+	});
 
-		palindrone.fn.init('this string has 4 palindromes in it, kayak, bob');
-		palindrone.fn.findPalindromicSequences();
-		expect(palindrone.foundPalindromicSequences.length)
-		.toBe(4);
+	it('should get a maximum of 3 palindromic sequences', function(){
+		expect(
+			palindrone('this string has 4 palindromes in it, kayak, bob').length
+		).toBe(3);
+
+		expect(
+			palindrone('no palindrome').length
+		).toBe(0);
 	});
 });
