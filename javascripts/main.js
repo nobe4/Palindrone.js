c2489ad1465e16ee587d7c93272fd545b38f2900
nobe4 <haffreinguev@gmail.com>
2015-12-16 00:15:06 +0100

    Update page with example

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+$(function(){
+  console.log(palindrone);
+  $('#palindrone-input').on('keyup', function(){
+    $('#palindrone-output').text(
+      JSON.stringify(
+      palindrone($(this).val())
+      , null, '\t')
+    );
+  });
+});
